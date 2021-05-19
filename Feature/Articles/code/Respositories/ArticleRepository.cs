107ef0a8c89b9cfc83496c646eabea592d92422a
agsh94 <agnihotri.shivang94@gmail.com>
2021-05-19 11:57:49 +0000

    Added ServiceAttribute, comments and logs

--- Feature/Articles/code/Respositories/ArticleRepository.cs
+++ Feature/Articles/code/Respositories/ArticleRepository.cs
@@ -5,15 +5,20 @@
     using System.Linq;
     using System.Web;
     using Sitecore.Data.Items;
+    using Sitecore.Diagnostics;
+    using Sitecore.Foundation.MultiTenant;
 
+    [Service(typeof(IArticleRepository))]
     public class ArticleRepository : IArticleRepository
     {
         public IEnumerable<Item> Get(Item contextItem)
         {
             if (contextItem == null)
             {
+                Log.Error("Item not found", contextItem);
                 throw new ArgumentNullException(nameof(contextItem));
             }
+            // return decendants of the current item.
             return contextItem.GetChildren().OrderBy(x => x.Name);
 
         }
@@ -21,20 +26,23 @@
         {
             if (contextItem == null)
             {
+                Log.Error("Item not found", contextItem);
                 throw new ArgumentNullException(nameof(contextItem));
             }
-
+            // return article details
             return contextItem;
         }
         public IEnumerable<Item> GetRelatedArticle(Item contextItem)
         {
             if (contextItem == null)
             {
+                Log.Error("Item not found", contextItem);
                 throw new ArgumentNullException(nameof(contextItem));
             }
             Data.Fields.MultilistField relatedArticles = contextItem?.Fields[Constants.Fields.RelatedArticlesField];
             Item[] items = relatedArticles?.GetItems();
 
+            // return selected articles' details
             return items.OrderBy(x => x.Name);
         }
     }
